06b77eb885f32737cdefa593550233288a4aaca3
fish515 <hxs356@case.edu>
2016-03-07 02:01:48 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,6 +1,7 @@
 data<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings = "?")
 subsetdata<- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 png("plot3.png", width=480, height=480)
+datetime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 plot(datetime,subsetdata$Sub_metering_1,type = "l",ylab="Energy sub metering",xlab="")
 lines(datetime,subsetdata$Sub_metering_2,type = "l",col="red")
 lines(datetime,subsetdata$Sub_metering_3,type = "l",col="blue")
